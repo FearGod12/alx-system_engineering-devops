692a4fe65df6bcb5c8de58ca654fbf9ede1e6fc9
FearGod12 <onyenikechukwudi@gmail.com>
2023-03-25 20:49:44 +0000

    task 100 fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #  using Puppet to make changes to our configuration file
 file { '/home/vagrant/.ssh/config':
   ensure  => present,
-  content => "Host 155255-web-01\n  Hostname 18.210.14.42\n  IdentityFile ~/.ssh/school\n  BatchMode yes > ~/.ssh/config",
+  content => "Host 155255-web-01\n  Hostname 18.210.14.42\n  IdentityFile ~/.ssh/school\n  BatchMode yes"
 }
