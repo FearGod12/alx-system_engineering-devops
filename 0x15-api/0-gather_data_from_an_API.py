b8141d411affd03f3325d4df7f5c65ae6e4deba5
FearGod12 <onyenikechukwudi@gmail.com>
2023-04-24 19:24:03 +0000

    fix requirement

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,8 +2,8 @@
 ''' using this REST API (https://jsonplaceholder.typicode.com), for a
 given employee ID,returns information about his/her TODO list progress'''
 
-from sys import argv
 import json
+from sys import argv
 from urllib.request import urlopen
 
 if __name__ == "__main__":
