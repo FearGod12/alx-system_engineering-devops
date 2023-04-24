d824384d2dd571dcd80dc4150fa92d02a7b3d643
FearGod12 <onyenikechukwudi@gmail.com>
2023-04-24 21:51:33 +0000

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,27 +6,24 @@ from urllib.request import urlopen
 
 if __name__ == "__main__":
     url_users = 'https://jsonplaceholder.typicode.com/users'
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
 
-    dump_data = {}
     users = []
     with urlopen(url_users) as response1:
         users = json.loads(response1.read().decode())
 
+    with urlopen(url_todo) as response:
+        todos = json.loads(response.read().decode())
+
+    dump_data = {}
     for user in users:
-        user_id = user.get("id")
-        url = url_users + '/' + str(user_id) + '/todos'
-        todos = []
-        with urlopen(url) as response:
-            todos = json.loads(response.read().decode())
-
         values = []
+        user_id = user.get("id")
         for each in todos:
-            data = {}
-            data["task"] = each.get("title")
-            data["completed"] = each.get("completed")
-            data["username"] = each.get("username")
-            values.append(data)
-
+            if user_id == each.get("userId"):
+                values.append({"task": each.get("title"),
+                               "completed": each.get("completed"),
+                               "username": user.get("username")})
         dump_data[user_id] = values
 
     with open("todo_all_employees.json", 'w') as f:
