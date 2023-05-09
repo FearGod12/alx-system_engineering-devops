bd1085870c00da03cd62b46021c308d13b2115b1
FearGod12 <onyenikechukwudi@gmail.com>
2023-05-09 19:58:10 +0000

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit'''
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''returns the number of subscribers in a given sureddit'''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"user-agent": "chuks agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.ok:
+        data = response.json().get("data", 0)
+        return data.get('subscribers', 0)
+    return response
